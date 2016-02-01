411970e2c2e0e11470824ec72b4aa5f1de2734ba
Valtteri Louhelainen <valtteri.louhelainen@gmail.com>
2016-02-01 22:32:54 +0200

    Added D6T3
    Demo 6 Teht 3

---
+++ D6T3/DeckOfCards/Card.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckOfCards
+{
+    class Card
+    {
+        //Suit: hearts + diamonds + clubs + spades
+        public string Suit { get; set; }
+        //Number: 2-14
+        public int Number { get; set; }
+        //Number name for 11-14: "Jack" + "Queen" + "King" + "Ace"
+        public string NumberName { get; set; }
+        
+
+        public override string ToString()
+        {
+            if (Number <= 10)
+            {
+                return Number + " of " + Suit;
+            }
+            else if (Number == 11) NumberName = "Jack";
+            else if (Number == 12) NumberName = "Queen";
+            else if (Number == 13) NumberName = "King";
+            else if (Number == 14) NumberName = "Ace";
+            return NumberName + " of " + Suit;
+        }
+    }
+}
